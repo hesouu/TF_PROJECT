675a3fe2d9b3e952da173185815cd7f84eb1cf1c
hesouu <hikari251025@gmail.com>
2026-07-27 13:31:36 +0900

    Feat: ec2 삭제 처리

--- basic/outputs.tf
+++ basic/outputs.tf
@@ -3,22 +3,22 @@
 
 # 임시코드, vpc, subnets 출력
 output "defalut_vpc_id" {
-    value = data.aws_vpc.default.id
-    description = "서울 리전의 기본 VPC의 id"
+  value       = data.aws_vpc.default.id
+  description = "서울 리전의 기본 VPC의 id"
 }
 output "defalut_subnets_id" {
-    value = data.aws_subnets.default.ids
-    description = "서울 리전의 기본 VPC에 속한 서브넷 id 리스트"
+  value       = data.aws_subnets.default.ids
+  description = "서울 리전의 기본 VPC에 속한 서브넷 id 리스트"
 }
 # 현재 위치 확인 ( ~/basic/ )
 # 출결결과 확인 (init -> plan -> apply -> destroy) 
 
 # 알파벳 순으로 출력된다 (작성 기준 x)
 output "aws_ami_maybejohn_linux_id" {
-    value = data.aws_ami.maybejohn_linux.id
-    description = "아마존 리눅스 AMI 아이디 조회"
+  value       = data.aws_ami.maybejohn_linux.id
+  description = "아마존 리눅스 AMI 아이디 조회"
 }
 output "aws_ami_maybejohn_linux_info" {
-    value = data.aws_ami.maybejohn_linux
-    description = "아마존 리눅스 AMI 조회"
+  value       = data.aws_ami.maybejohn_linux
+  description = "아마존 리눅스 AMI 조회"
 }

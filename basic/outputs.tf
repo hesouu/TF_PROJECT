81bb556b722848ae79a954afd22a22876c391165
hesouu <hikari251025@gmail.com>
2026-07-27 13:05:56 +0900

    Feat: AMI 이미지 정보 조회

--- basic/outputs.tf
+++ basic/outputs.tf
@@ -14,7 +14,11 @@ output "defalut_subnets_id" {
 # 출결결과 확인 (init -> plan -> apply -> destroy) 
 
 # 알파벳 순으로 출력된다 (작성 기준 x)
-output aws_ami_maybejohn_linux_id {
+output "aws_ami_maybejohn_linux_id" {
     value = data.aws_ami.maybejohn_linux.id
     description = "아마존 리눅스 AMI 아이디 조회"
+}
+output "aws_ami_maybejohn_linux_info" {
+    value = data.aws_ami.maybejohn_linux
+    description = "아마존 리눅스 AMI 조회"
 }

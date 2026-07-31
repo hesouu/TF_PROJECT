1c9576b96c641a1fc57c7cd0d7de306e9cd6213c
hesouu <hikari251025@gmail.com>
2026-07-31 09:31:09 +0900

    Feat: 향후 Version 구성 정리

--- tf-step3/basic/main.tf
+++ tf-step3/basic/main.tf
@@ -14,11 +14,11 @@ data "aws_subnets" "default" {
 }
 
 # 3. 보안그룹 생성 선언 - EC2 진입 하는데 인바운드 IP/포트, 아웃바운드 IP/포트 설정 => 접근 제한!!
-resource "aws_security_group" "DE-AI-25-IaC-TF-GROUP" {
+resource "aws_security_group" "DE-AI-09-IaC-TF-GROUP" {
   # 메타 정보
-  name = "terraform-25-sg2"
+  name = "terraform-09-sg2"
   # ASCII만 지원
-  description = "de-ai-25 security group"
+  description = "de-ai-09 security group"
   # 보안 그룹은 VPC에 종속되어서 구성됨
   # id => 리소스명-해시값(중복x, 고유값)
   vpc_id = data.aws_vpc.default.id
@@ -28,12 +28,12 @@ resource "aws_security_group" "DE-AI-25-IaC-TF-GROUP" {
     from_port   = 22
     to_port     = 22
     description = "SSH"
-    # 0.0.0.0/0 => 각자리가 256개 표현(0~255).().().()/앞에서부터 고정값(비트수:0,8,16,24,32)
-    # 256*256*256*256 개 주소 표현
-    # 10.0.0.0/8 => 맨 앞에 1자리는 고정 => 10은 고정 나머지 3자리에서 모두 가능 => 256*256*256 주소 가능함
+    # 0.0.0.0/0 => 각자리가 096개 표현(0~095).().().()/앞에서부터 고정값(비트수:0,8,16,24,32)
+    # 096*096*096*096 개 주소 표현
+    # 10.0.0.0/8 => 맨 앞에 1자리는 고정 => 10은 고정 나머지 3자리에서 모두 가능 => 096*096*096 주소 가능함
     # 0.0.0.0/0 => Anywhere IPV4 (전세계 어디서든 접근 가능 -> 보안에 좋진 않다)
-    # 222.108.125.33/32 => 오직 이 IP만 접속 가는함!! ~/32(4자리 모두 고정)
-    # cidr_blocks = ["222.108.125.33/32"]
+    # 222.108.109.33/32 => 오직 이 IP만 접속 가는함!! ~/32(4자리 모두 고정)
+    # cidr_blocks = ["222.108.109.33/32"]
     # 웹 브라우저 접속시 전부 개방
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -76,7 +76,7 @@ data "aws_ami" "amazon_linux" {
 }
 
 # 4. EC2 생성 선언
-resource "aws_instance" "DE-AI-25-IaC-TF" {
+resource "aws_instance" "DE-AI-09-IaC-TF" {
   # AMI -> OS
   ami = data.aws_ami.amazon_linux.id
   # 인스턴스 유형
@@ -87,13 +87,13 @@ resource "aws_instance" "DE-AI-25-IaC-TF" {
   subnet_id = data.aws_subnets.default.ids[0] # a,b,c,d중 첫번재 선택(a)
   # 보안그룹
   vpc_security_group_ids = [
-    aws_security_group.DE-AI-25-IaC-TF-GROUP.id
+    aws_security_group.DE-AI-09-IaC-TF-GROUP.id
   ]
   # 스토리지 생략
   # 고급 설정 생략
   # 태그
   tags = {
-    Name = "DE-AI-25-IaC-TF-EC2"
+    Name = "DE-AI-09-IaC-TF-EC2"
   }
   # IP는 임시로 자동할당 (현재 EIP 사용 x)
   # AMI에서 아마존리눅스 최신버전 획득 => 웹 연결 브라우저 접속 기능 설치 x 있음
@@ -108,9 +108,9 @@ resource "aws_instance" "DE-AI-25-IaC-TF" {
 }
 
 # 5. Elastic IP 생성 선언 -> EC2 연결 (차후 IP를 미리 확보하고 ec2 생성되면 연결하는 방식 진화)
-# resource "aws_eip" "DE-AI-25-IaC-TF-EIP" {
+# resource "aws_eip" "DE-AI-09-IaC-TF-EIP" {
 #   # EC2 인스턴스 
-#   instance = aws_instance.DE-AI-25-IaC-TF.id
+#   instance = aws_instance.DE-AI-09-IaC-TF.id
 #   # 네트워크
 #   domain = "vpc"
 # }

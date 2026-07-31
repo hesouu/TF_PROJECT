f2ca0e2a2ded62c5f55722f3d872c12bf6946161
hesouu <hikari251025@gmail.com>
2026-07-31 15:46:45 +0900

    Feat: 우히히히히ㅣㅎ

--- tf-step5/launch_template.tf
+++ tf-step5/launch_template.tf
@@ -4,7 +4,33 @@
 resource "aws_launch_template" "web" {
   name_prefix = "${local.project}-WEB-" # 증감이 수시로 방생해도 중복 x
   image_id    = data.aws_ami.amazon_linux
+  instance_type = var.instance_type
+  # SSM 접속을 위해 프로파일 설정 -> iam.tf 구성
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ec2_ssm.name
+  }
+  # 보안 그룹 지정
+  vpc_security_group_ids =  [aws_security_group.web.id]
+  # 서버 구성후 초기 작업 -> 쉘스크립트를 읽어서 -> base64 인코딩처리 -> 실행되게 구성
+  user_data = base64encode( templatefile("${path.module}/userdata-web.sh.tftpl", {
+    # WEB -> proxy -> InTernal ALB (이 리소스의 IP 혹은 dns 알아야 전달)
+    # 해당 값을 가져가서 ㅜ햐ㅜㅌ conf 파일을 완성함
+    internal_alb_dns = aws_lb.internal.dns_name
+  }) )
+  # 태그
+  tag_specifications {
+    resource_type = "instance"
+    tags = merge(local.common_tags, {
+        Name = "${local.project}-WEB"
+        Tier = "web"
+    })
+  }
+  # 삭제전 생성 -> ec2 수 유지에 대한 기준
+  lifecycle {
+    create_before_destroy = true
+  }
 }
+
 
 
 #################################################################

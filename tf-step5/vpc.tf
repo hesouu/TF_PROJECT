d278c76f99de1407d48de2b7182adfa5c27e14c3
hesouu <hikari251025@gmail.com>
2026-07-31 11:30:23 +0900

    Feat: vpc, igw 구성 완료

--- tf-step5/vpc.tf
+++ tf-step5/vpc.tf
@@ -1,8 +1,24 @@
 # VPC
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    Name = "${local.project}-vpc"
+  }
+}
 
 # Internet Gateway
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.mian.id
+  tags = {
+    Name = "${local.project}-IIGWW"
+  }
+}
 
 # Public Subnets - Public ALB, NAT Gateway
+
+
 
 # Private Application Subnets - Web,  Was, internal ALB
 

05b46e39d3e8ca97c16a8d350f0a178441da76cc
hesouu <hikari251025@gmail.com>
2026-07-31 13:19:59 +0900

    Feat: VPC-PUBLIC Route Table 생성 및 서브넷 연결

--- tf-step4/vpc.tf
+++ tf-step4/vpc.tf
@@ -101,7 +101,7 @@ resource "aws_route_table" "private" {
 }
 
 # 최종연결 (프라이빗 서브넷 -> 프라이빗전용 라우트테이블)
-resource "aws_route_table_association" "private" {
-  subnet_id      = aws_subnet.private.id
-  route_table_id = aws_route_table.private.id
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
 }
--- tf-step5/vpc.tf
+++ tf-step5/vpc.tf
@@ -64,6 +64,22 @@ resource "aws_subnet" "db" {
 }
 
 # Public Route Table/association
+resource "aws_route_table" "private" {
+  vpc_id       = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.main.id
+  }
+  tags = {
+    Name       = "${local.project}-PUBLIC-RT"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  for_each       = aws_subnet.public
+  subnet_id      = each.value.id # a 존의 서브냇, c 존의 서브넷 => 반복 구성 연결
+  route_table_id = aws_route_table.public.id
+}
 
 # Nate Gateway - eip
 

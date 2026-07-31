e37796ef98bc075acee36ac3b92ee54902c781bd
hesouu <hikari251025@gmail.com>
2026-07-31 15:53:15 +0900

    Feat: 런치 템플릿 구성 완료, 단 rds 예정

--- tf-step5/launch_template.tf
+++ tf-step5/launch_template.tf
@@ -3,7 +3,7 @@
 #################################################################
 resource "aws_launch_template" "web" {
   name_prefix = "${local.project}-WEB-" # 증감이 수시로 방생해도 중복 x
-  image_id    = data.aws_ami.amazon_linux
+  image_id    = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type
   # SSM 접속을 위해 프로파일 설정 -> iam.tf 구성
   iam_instance_profile {
@@ -14,7 +14,7 @@ resource "aws_launch_template" "web" {
   # 서버 구성후 초기 작업 -> 쉘스크립트를 읽어서 -> base64 인코딩처리 -> 실행되게 구성
   user_data = base64encode( templatefile("${path.module}/userdata-web.sh.tftpl", {
     # WEB -> proxy -> InTernal ALB (이 리소스의 IP 혹은 dns 알아야 전달)
-    # 해당 값을 가져가서 ㅜ햐ㅜㅌ conf 파일을 완성함
+    # 해당 값을 가져가서 nginx conf 파일을 완성함
     internal_alb_dns = aws_lb.internal.dns_name
   }) )
   # 태그
@@ -36,3 +36,31 @@ resource "aws_launch_template" "web" {
 #################################################################
 # was ec2용 -> launch template 요소(ASG 내에서 사용)를 사용하여 생성됨
 #################################################################
+resource "aws_launch_template" "was" {
+  name_prefix = "${local.project}-WAS-"
+  image_id    = data.aws_ami.amazon_linux.id
+  instance_type = var.instance_type
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ec2_ssm.name
+  }
+  vpc_security_group_ids =  [aws_security_group.was.id]
+  user_data = base64encode( templatefile("${path.module}/userdata-was.sh.tftpl", {
+    # rds 세팅값 설정
+    db_host = 
+    db_port =
+    db_name = var.db_name
+    db_user = var.db_username
+  }) )
+  # 태그
+  tag_specifications {
+    resource_type = "instance"
+    tags = merge(local.common_tags, {
+        Name = "${local.project}-WAS"
+        Tier = "was"
+    })
+  }
+  
+  lifecycle {
+    create_before_destroy = true
+  }
+}
